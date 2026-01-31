260e160d1dbde392a372637eeaf4f31c27bcfedd
Diogodominguez64-blip <diogodominguez64@gmail.com>
2026-01-31 12:11:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let cart=[];
 let rate={USD:1,MXN:17,COP:3900,PEN:3.7,ARS:850};
 
-function addProduct(name,selectId){
-let sel=document.getElementById(selectId);
+function addProduct(name,id){
+let sel=document.getElementById(id);
 let [price,plan]=sel.value.split("|");
-cart.push({name,plan,price:parseFloat(price)});
+cart.push({name,plan,price:+price});
 render();
 toast();
 window.scrollTo({top:document.body.scrollHeight,behavior:"smooth"});
@@ -29,23 +29,26 @@ ${p.name} (${p.plan}) - ${p.price} USD
 });
 document.getElementById("count").innerText=cart.length;
 document.getElementById("total").innerText=total.toFixed(2);
+document.getElementById("cartBox")
+.classList.toggle("glow",cart.length>0);
 }
 
 function convert(){
 let cur=document.getElementById("currency").value;
 let total=cart.reduce((a,b)=>a+b.price,0);
-document.getElementById("total").innerText=(total*rate[cur]).toFixed(2)+" "+cur;
+document.getElementById("total").innerText=
+(total*rate[cur]).toFixed(2)+" "+cur;
 }
 
 function openTicket(){
-if(cart.length===0)return;
+if(!cart.length)return;
 let s=document.getElementById("seller").value;
 if(!s)return;
 let [name,phone]=s.split("|");
 let total=cart.reduce((a,b)=>a+b.price,0);
-let msg=`🧾 FACTURA DZ STORE\n\n👤 Vendedor: ${name}\n📦 Productos:\n`;
+let msg=`🧾 FACTURA ZSTORE\n\n👤 Vendedor: ${name}\n`;
 cart.forEach(p=>msg+=`• ${p.name} (${p.plan}) - ${p.price} USD\n`);
-msg+=`\n💵 Total: ${total} USD\n🕒 ${new Date().toLocaleString()}\n\nGracias por tu compra 💚`;
+msg+=`\n💵 Total: ${total} USD\n🕒 ${new Date().toLocaleString()}`;
 window.open(`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`,"_blank");
 cart=[];
 render();
